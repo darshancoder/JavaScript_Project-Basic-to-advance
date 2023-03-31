ce8c4837d84ab4d38e4246297b22e2f31e032083
Darshan Kale <darshankale52@gmail.com>
2023-03-31 11:34:29 +0530

    Product Search Engine Done

---
+++ Search_Engine/app.js
@@ -0,0 +1,21 @@
+const search = () => {
+    const searchBox = document.getElementById("search-item").value.toUpperCase()
+    const storeItems = document.getElementById("product-list")
+    const product = document.querySelectorAll(".product")
+    const pname = document.getElementsByTagName("h2")
+
+    for(let i=0; i<pname.length; i++){
+        let match = product[i].getElementsByTagName("h2")[0]
+        
+        if(match){
+         let textValue =   match.textContent || match.innerHTML
+
+         if(textValue.toUpperCase().indexOf(searchBox) > -1){
+            product[i].style.display = "";
+         }else{
+            product[i].style.display = "none";
+         }
+        }
+    }
+}
+
