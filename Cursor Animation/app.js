3a20a1ca387d990f11f537bc2381fe3b427fc6a9
Darshan Kale <darshankale52@gmail.com>
2023-04-04 14:52:36 +0530

    Cursor Animation

---
+++ Cursor Animation/app.js
@@ -0,0 +1,23 @@
+const cursor = document.querySelector(".cursor")
+var timeOut;
+
+document.addEventListener("mousemove", (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top =  y + 'px'
+    cursor.style.left = x + 'px'
+    cursor.style.display = 'block';
+
+    function mouseStopped() {
+        cursor.style.display = 'none'
+    }
+    // Hide our animation
+    clearTimeout(timeOut)
+    timeOut = setTimeout(mouseStopped, 1000)
+})
+
+// /Stop animation off the screen 
+document.addEventListener("mouseout", () => {
+    cursor.style.display = 'none'
+})
