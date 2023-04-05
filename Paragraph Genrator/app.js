6541e7a0948396d9d105b7e4e066ace2e429bfa2
Darshan Kale <darshankale52@gmail.com>
2023-04-05 23:09:11 +0530

    Random Paragraph Generator Done

---
+++ Paragraph Genrator/app.js
@@ -0,0 +1,83 @@
+const para = [
+  "The sun was setting behind the mountains, casting a warm glow across the fields. Birds were singing in the trees and a light breeze rustled the leaves. It was a peaceful evening and the perfect end to a busy day.Sarah walked along the beach, feeling the sand between her toes and the salty sea air on her face. She watched the waves crash against the shore and listened to the seagulls calling in the distance. It was her favourite place to be and she felt at home by the sea.",
+  "The city was alive with noise and activity, people rushing to and fro, cars honking their horns and sirens wailing in the distance. It was a chaotic scene, but somehow it all worked together to create a vibrant energy that was both exciting and exhausting.",
+  "The forest was quiet, except for the occasional rustle of leaves as a small animal scurried by. The trees towered above, their branches reaching towards the sky, and sunlight filtered down through the canopy. It was a peaceful and serene place, where one could escape the hustle and bustle of the city and find solace in nature.",
+  "The old house stood at the end of the road, its paint peeling and windows boarded up. It was a spooky sight, but also intriguing. There were rumours of ghosts and strange happenings, and many people avoided the house altogether. But for some, the mystery was too tempting to resist.",
+  "The sun was setting behind the mountains, casting a warm glow across the fields. Birds were singing in the trees and a light breeze rustled the leaves. It was a peaceful evening and the perfect end to a busy day.",
+  "Sarah walked along the beach, feeling the sand between her toes and the salty sea air on her face. She watched the waves crash against the shore and listened to the seagulls calling in the distance. It was her favourite place to be and she felt at home by the sea.",
+  "The city was alive with noise and activity, people rushing to and fro, cars honking their horns and sirens wailing in the distance. It was a chaotic scene, but somehow it all worked together to create a vibrant energy that was both exciting and exhausting.",
+  "The forest was quiet, except for the occasional rustle of leaves as a small animal scurried by. The trees towered above, their branches reaching towards the sky, and sunlight filtered down through the canopy. It was a peaceful and serene place, where one could escape the hustle and bustle of the city and find solace in nature.",
+  "The old house stood at the end of the road, its paint peeling and windows boarded up. It was a spooky sight, but also intriguing. There were rumours of ghosts and strange happenings, and many people avoided the house altogether. But for some, the mystery was too tempting to resist.",
+  "In some circles, Ben Hogan is credited with “inventing practice. Hogan was one of the greatest golfers of the 20th century, an accomplishment he achieved through tireless repetition. He simply loved to practice. Hogan said, “I couldn't wait to get up in the morning so I could hit balls. I'd be at the practice tee at the crack of dawn, hit balls for a few hours, then take a break and get right back to it.",
+
+"For Hogan, every practice session had a purpose. He reportedly spent years breaking down each phase of the golf swing and testing new methods for each segment. The result was near perfection. He developed one of the most finely-tuned golf swings in the history of the game.His precision made him more like a surgeon than a golfer. During the 1953 Masters, for example, Hogan hit the flagstick on back-to-back holes. A few days later, he broke the tournament scoring record.Ben Hogan's 1 iron shot at the 1950 US Open by Hy Peskin Hogan methodically broke the game of golf down into chunks and figured out how he could master each section. For example, he was one of the first golfers to assign specific yardages to each golf club. Then, he studied each course carefully and used trees and sand bunkers as reference points to inform him about the distance of each shot.",
+
+"Hogan finished his career with nine major championships—ranking fourth all-time. During his prime, other golfers simply attributed his remarkable success to “Hogan's secret.” Today, experts have a new term for his rigorous style of improvement: deliberate practice.",
+
+"What is Deliberate Practice? => Deliberate practice refers to a special type of practice that is purposeful and systematic. While regular practice might include mindless repetitions, deliberate practice requires focused attention and is conducted with the specific goal of improving performance. When Ben Hogan carefully reconstructed each step of his golf swing, he was engaging in deliberate practice. He wasn't just taking cuts. He was finely tuning his technique.",
+
+"While regular practice might include mindless repetitions, deliberate practice requires focused attention and is conducted with the specific goal of improving performance. The greatest challenge of deliberate practice is to remain focused. In the beginning, showing up and putting in your reps is the most important thing. But after a while we begin to carelessly overlook small errors and miss daily opportunities for improvement.",
+
+"This is because the natural tendency of the human brain is to transform repeated behaviors into automatic habits. For example, when you first learned to tie your shoes you had to think carefully about each step of the process. Today, after many repetitions, your brain can perform this sequence automatically. The more we repeat a task the more mindless it becomes.",
+
+"Mindless activity is the enemy of deliberate practice. The danger of practicing the same thing again and again is that progress becomes assumed. Too often, we assume we are getting better simply because we are gaining experience. In reality, we are merely reinforcing our current habits—not improving them.",
+
+"Claiming that improvement requires attention and effort sounds logical enough. But what does deliberate practice actually look like in the real world? Let's talk about that now. Examples of Deliberate Practice One of my favorite examples of deliberate practice is discussed in Talent is Overrated by Geoff Colvin. In the book, Colvin describes how Benjamin Franklin used deliberate practice to improve his writing skills.",
+"The sun was setting behind the mountains, casting a warm glow across the fields. Birds were singing in the trees and a light breeze rustled the leaves. It was a peaceful evening and the perfect end to a busy day.Sarah walked along the beach, feeling the sand between her toes and the salty sea air on her face. She watched the waves crash against the shore and listened to the seagulls calling in the distance. It was her favourite place to be and she felt at home by the sea.",
+"The city was alive with noise and activity, people rushing to and fro, cars honking their horns and sirens wailing in the distance. It was a chaotic scene, but somehow it all worked together to create a vibrant energy that was both exciting and exhausting.",
+"The forest was quiet, except for the occasional rustle of leaves as a small animal scurried by. The trees towered above, their branches reaching towards the sky, and sunlight filtered down through the canopy. It was a peaceful and serene place, where one could escape the hustle and bustle of the city and find solace in nature.",
+"The old house stood at the end of the road, its paint peeling and windows boarded up. It was a spooky sight, but also intriguing. There were rumours of ghosts and strange happenings, and many people avoided the house altogether. But for some, the mystery was too tempting to resist.",
+"The sun was setting behind the mountains, casting a warm glow across the fields. Birds were singing in the trees and a light breeze rustled the leaves. It was a peaceful evening and the perfect end to a busy day.",
+"Sarah walked along the beach, feeling the sand between her toes and the salty sea air on her face. She watched the waves crash against the shore and listened to the seagulls calling in the distance. It was her favourite place to be and she felt at home by the sea.",
+"The city was alive with noise and activity, people rushing to and fro, cars honking their horns and sirens wailing in the distance. It was a chaotic scene, but somehow it all worked together to create a vibrant energy that was both exciting and exhausting.",
+"The forest was quiet, except for the occasional rustle of leaves as a small animal scurried by. The trees towered above, their branches reaching towards the sky, and sunlight filtered down through the canopy. It was a peaceful and serene place, where one could escape the hustle and bustle of the city and find solace in nature.",
+"The old house stood at the end of the road, its paint peeling and windows boarded up. It was a spooky sight, but also intriguing. There were rumours of ghosts and strange happenings, and many people avoided the house altogether. But for some, the mystery was too tempting to resist.",
+"In some circles, Ben Hogan is credited with “inventing practice. Hogan was one of the greatest golfers of the 20th century, an accomplishment he achieved through tireless repetition. He simply loved to practice. Hogan said, “I couldn't wait to get up in the morning so I could hit balls. I'd be at the practice tee at the crack of dawn, hit balls for a few hours, then take a break and get right back to it.",
+
+"For Hogan, every practice session had a purpose. He reportedly spent years breaking down each phase of the golf swing and testing new methods for each segment. The result was near perfection. He developed one of the most finely-tuned golf swings in the history of the game.His precision made him more like a surgeon than a golfer. During the 1953 Masters, for example, Hogan hit the flagstick on back-to-back holes. A few days later, he broke the tournament scoring record.Ben Hogan's 1 iron shot at the 1950 US Open by Hy Peskin Hogan methodically broke the game of golf down into chunks and figured out how he could master each section. For example, he was one of the first golfers to assign specific yardages to each golf club. Then, he studied each course carefully and used trees and sand bunkers as reference points to inform him about the distance of each shot.",
+
+"Hogan finished his career with nine major championships—ranking fourth all-time. During his prime, other golfers simply attributed his remarkable success to “Hogan's secret.” Today, experts have a new term for his rigorous style of improvement: deliberate practice.",
+
+"What is Deliberate Practice? => Deliberate practice refers to a special type of practice that is purposeful and systematic. While regular practice might include mindless repetitions, deliberate practice requires focused attention and is conducted with the specific goal of improving performance. When Ben Hogan carefully reconstructed each step of his golf swing, he was engaging in deliberate practice. He wasn't just taking cuts. He was finely tuning his technique.",
+
+"While regular practice might include mindless repetitions, deliberate practice requires focused attention and is conducted with the specific goal of improving performance. The greatest challenge of deliberate practice is to remain focused. In the beginning, showing up and putting in your reps is the most important thing. But after a while we begin to carelessly overlook small errors and miss daily opportunities for improvement.",
+
+"This is because the natural tendency of the human brain is to transform repeated behaviors into automatic habits. For example, when you first learned to tie your shoes you had to think carefully about each step of the process. Today, after many repetitions, your brain can perform this sequence automatically. The more we repeat a task the more mindless it becomes.",
+
+"Mindless activity is the enemy of deliberate practice. The danger of practicing the same thing again and again is that progress becomes assumed. Too often, we assume we are getting better simply because we are gaining experience. In reality, we are merely reinforcing our current habits—not improving them.",
+
+"Claiming that improvement requires attention and effort sounds logical enough. But what does deliberate practice actually look like in the real world? Let's talk about that now. Examples of Deliberate Practice One of my favorite examples of deliberate practice is discussed in Talent is Overrated by Geoff Colvin. In the book, Colvin describes how Benjamin Franklin used deliberate practice to improve his writing skills."
+];
+
+const item = document.querySelector("#items");
+const dataContainer = document.querySelector("#data");
+const generate = () => {
+  if (isNaN(item.value) || item.value < 0 || item.value > para.length) {
+    const random = Math.floor(Math.random() * para.Plength);
+    dataContainer.innerHTML = `<p>${para[random]}</p>`
+    
+        // alert(`Successfully Generate Your Pragraph ${para.length}`)
+        if(item.value !== undefined){
+            alert(`Please Enter Near About this ${para.length}`)
+            dataContainer.innerHTML = `<h2 class="alert">You don't have that much data That Enter near About <span>${para.length}</span></h2>`
+        }
+        console.log("1")
+    
+  }else{
+    
+    const data = para.slice(0,item.value)
+    const paras = data.map(
+        (d) => {
+            return `<p>${d}</p>`
+        }
+    )
+    dataContainer.innerHTML = paras.join("");
+    alert(`Successfully Generate Your Pragraph ${data.length}`)
+
+    if(data.length === undefined){
+        alert(`Please Enter Near About this ${para.length}`)
+    }
+    console.log("2")
+
+  }
+};
