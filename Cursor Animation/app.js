f58dcdb4c083cd30a3f848b344de8dfe200e9fe9
Darshan Kale <darshankale52@gmail.com>
2023-04-05 09:35:10 +0530

    Animted Website

--- Cursor Animation/app.js
+++ Cursor Animation/app.js
@@ -1,23 +1,23 @@
-const cursor = document.querySelector(".cursor")
+const cursor = document.querySelector(".cursor");
 var timeOut;
 
 document.addEventListener("mousemove", (e) => {
-    let x = e.pageX;
-    let y = e.pageY;
+  let x = e.pageX;
+  let y = e.pageY;
 
-    cursor.style.top =  y + 'px'
-    cursor.style.left = x + 'px'
-    cursor.style.display = 'block';
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
 
-    function mouseStopped() {
-        cursor.style.display = 'none'
-    }
-    // Hide our animation
-    clearTimeout(timeOut)
-    timeOut = setTimeout(mouseStopped, 1000)
-})
+  function mouseStopped() {
+    cursor.style.display = "none";
+  }
+  // Hide our animation
+  clearTimeout(timeOut);
+  timeOut = setTimeout(mouseStopped, 1000);
+});
 
-// /Stop animation off the screen 
+// /Stop animation off the screen
 document.addEventListener("mouseout", () => {
-    cursor.style.display = 'none'
-})
+  cursor.style.display = "none";
+});
